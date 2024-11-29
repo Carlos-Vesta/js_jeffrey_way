13a8f42fb265ebb912c917544aa0511e77149781
Carlos Vesta <vesta.carlos@gmail.com>
2024-11-29 17:42:54 +0200

    03.data_and_variables

---
+++ 01.fundamentals_101/01.basics/js/03.data_and_variables.js
@@ -0,0 +1,11 @@
+
+// Define multiple variables in single statement
+var sum = 4 + 5, // 9
+    difference = 4 - 5, // -1
+    product = 4 * 5, // 20
+    quotient = 4 / 5; // 0.8
+
+
+
+var foo = 4 - 5 * (6 - 8),
+    bar = foo + 1;
