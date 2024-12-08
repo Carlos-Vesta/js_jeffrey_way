af0074e0ad5be0265ca07bd400233f0e6a1e281c
Carlos Vesta <vesta.carlos@gmail.com>
2024-12-08 12:37:16 +0200

    14.conditions_and_decisions

---
+++ 01.fundamentals_101/01.basics/js/14.conditions_and_decisions.js
@@ -0,0 +1,33 @@
+
+/**
+ * ==== Logical AND (&&) ====
+ * Do you like cake AND pie?
+ * 
+ * true && true = true
+ * true && false = false
+ * false && true = false
+ * false && false = false
+ * 
+ * 
+ * 
+ * ==== Logical OR (||) ====
+ * Do you like cake OR pie?
+ * 
+ * true || true = true
+ * true || false = true
+ * false || true = true
+ * false || false = false
+ * 
+ * 
+ * 
+ * ==== Logical NOT (!) ====
+ * !true = false
+ * !false = true
+ */
+
+var foo = "hello";
+var bar =  5;
+
+if (foo !== "hello") {
+    console.log("true!");
+};
